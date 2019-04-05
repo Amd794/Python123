7e8f8a638cab72ae179df1c2d3a107ed78ac07b7
Mifen <2952277346@qq.com>
2019-04-05 21:02:09 +0800

    python123示例

---
+++ 康托尔集-深度大,相距间隔短..py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Time    : 2019/4/5 17:57
+# Author  : Mifen
+# Email   : 2952277346@qq.com
+# Software: PyCharm
---
+++ 科赫雪花.py
@@ -0,0 +1,70 @@
+import turtle as t
+from turtle import *
+import random
+
+
+
+def draw_path(path):
+    t.color(255,255,255)
+    for symbol in path:
+        if symbol == 'F':
+            forward(length)
+        elif symbol == '-':
+            right(angle)
+        elif symbol == '+':
+            left(angle)
+
+def apply_rule(path):
+    rule = 'F+F--F+F'
+    return path.replace('F',rule)
+
+length = .5
+angle  = 60
+setup(1280,720)
+bgcolor('black')
+up()
+color("#0fe6ca")
+goto(-600, 300)
+write('Author:Mifen', font=("微软雅黑", 18))
+goto(-600, 250)
+write('E-mail :2952277346@qq.com', font=("微软雅黑", 18))
+goto(0,0)
+down()
+path = 'F--F--F'
+speed(0)
+up()
+goto(-440,-250)
+down()
+for i in range(5):
+    path = apply_rule(path)
+draw_path(path)
+draw_path(path)
+draw_path(path)
+a,b = pos()
+for i in range(3):
+    up()
+    a += 250
+    goto(a,b)
+    down()
+    draw_path(path)
+    draw_path(path)
+    draw_path(path)
+b += 220
+for i in range(2):
+    up()
+    a -= 250
+    goto(a,b)
+    down()
+    draw_path(path)
+    draw_path(path)
+    draw_path(path)
+b += 220
+for i in range(2):
+
+    draw_path(path)
+    draw_path(path)
+    draw_path(path)
+    up()
+    a += 130
+    goto(a,b)
+    down()
